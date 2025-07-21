5739da2d77a00f85efbb688f80218228c27fb85f
technicalxakash <akashr2468@gmail.com>
2025-07-21 19:47:11 +0530

    new

--- src/www/home/programs/kRotateAnArray.java
+++ src/www/home/programs/kRotateAnArray.java
@@ -5,11 +5,12 @@ package www.home.programs;
 public class kRotateAnArray {
 
 	public static void main(String[] args) {
-		int[] a= {1,2,3,4,5,6,7};
+		int[] a= {10,20,30,40,50,60,70,80,90};
 		
 		
+		int n=4;
 		
-		int k=4;
+		int k=n+1;
 		
 		int[] aa = new int[k-1];
 		
@@ -74,6 +75,52 @@ while(iii<jjj) {
 
 for(int i1i:ab1) {
 	System.out.print(i1i+" ");
+}
+
+
+System.out.println();
+
+int [] ca=new int[a.length];
+
+
+for(int i2=0;i2<aa.length;i2++) {
+	
+	ca[i2]=aa[i2];
+}
+
+for(int i3=0;i3<ab1.length;i3++) {
+	
+	ca[aa.length+i3]=ab1[i3];
+	
+}
+
+for(int num:ca) {
+	System.out.print(num+" ");
+}
+
+System.out.println();
+
+int i9=0;
+
+int j9=ca.length-1;
+
+int temppp=0;
+
+while(i9<j9) {
+	
+	temppp=ca[i9];
+	
+	ca[i9]=ca[j9];
+	
+	ca[j9]=temppp;
+	
+	i9++;
+	j9--;
+
+}
+System.out.print(n+" "+"Rotated array is : ");
+for(int num1:ca) {
+	System.out.print(num1+" ");
 }
 
 
